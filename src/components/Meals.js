c5ebad94013e783efe88ddc3960e0b5814af870b
Git Server Admin <admin@git-server>
2019-12-08 16:49:48 +0000

    Add initial repository

---
+++ src/components/Meals.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {PropTypes} from 'prop-types';
+
+const Meals = (() => {
+    return (<div className="col-xs-12  col-sm-12 col-md-12 col-lg-12">
+            <ol id="list">
+                        <li className="morning">Breakfast for insert_person_name</li>
+                        <li className="afternoon">Lunch for insert_person_name</li>
+                        <li className="night">Dinner for insert_person_name</li>
+            </ol>
+        </div>);
+});
+export default Meals;
